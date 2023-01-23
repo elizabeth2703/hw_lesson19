9c7ca92f964d0943545cf215bb3ffd062504fd27
hw19 <elizavetaryzhankova@gmail.com>
2023-01-23 22:35:51 +0300

    addit fuatures or

--- task 1.py
+++ task 1.py
@@ -1,21 +1,23 @@
-def debug(function):
+from typing import Callable
+from functools import wraps
+def debug(name_function: Callable)->Callable:
+    @wraps(name_function)
     def wrapper(*args, **kwargs):
-        print("Вызывается {}({})".format(
-            function.__name__,
-            ", ".join(
-                list(f"\"{arg}\""
-                     if isinstance(arg, str) else
-                     str(arg) for arg in args)
-                +
-                list(f"{k}=\"{v}\""
-                     if isinstance(v, str) else
-                     f"{k}={v}" for k, v in kwargs.items())
-            )
-        ))
-        result = function(*args, **kwargs)
-        print("'{}' вернула значение'{}'".format(
-            function.__name__, result
-        ))
-        print(result)
-        return result
+        arg_kwarg_line = ''
+        for arg in args:
+            if isinstance(arg, str):
+                arg_kwarg_line += "".join([f"'{arg}'"])
+            else:
+                arg_kwarg_line += str(arg)
+        for key, value in kwargs.items():
+            if isinstance(value, str):
+                arg_kwarg_line += f"{key}='{value}'"
+            else:
+                arg_kwarg_line += f', {key}={value}'
+
+        print(f'Вызывается {name_function.__name__}({arg_kwarg_line})')
+        function_call = name_function(*args, **kwargs)
+        print(f"{name_function.__name__} вернула значение '{function_call}'")
+        return f'{function_call}\n'
+
     return wrapper
